6d2e98ed19b54c6ed48887d0e01c4e6b3c0cd2a2
Динара <zaripovadin@yandex.ru>
2022-06-30 01:20:26 +0300

    first commit

---
+++ app/src/main/java/com/example/summerprac22/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.summerprac22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.btn_calc)
+        button.setOnClickListener {
+            val editTextName = findViewById<EditText>(R.id.et_name)
+            val editTextHeight = findViewById<EditText>(R.id.et_height)
+            val editTextWeight = findViewById<EditText>(R.id.et_weight)
+            val editTextAge = findViewById<EditText>(R.id.et_age)
+            val result = findViewById<TextView>(R.id.tv_result)
+            if (editTextName.error == null && editTextHeight.error == null && editTextWeight.error == null
+                && editTextAge.error == null) {
+                val name:String = editTextName.text.toString()
+                val height: Int = editTextHeight?.text.toString().toInt()
+                val weight: Double = editTextWeight?.text.toString().toDouble()
+                val age: Int = editTextAge?.text.toString().toInt()
+
+                if ( name == "" || name.length > 50 || height <= 0 || height >= 250 || weight <= 0.0
+                    || weight >= 250.0 || age <= 0 || age >= 150){
+                    result.text = "Validation failed"
+                }else {
+                    result.text = calculate(name, height, weight, age)
+                }
+            } else {
+                result.text = "Form shouldn't be null"
+            }
+            result.visibility = View.VISIBLE
+        }
+    }
+    private fun calculate(name: String, height: Int, weight: Double, age: Int): String{
+        val result = (weight * 10 + height * 6.25 - age * 5 - 83)*1.2 //суточная норма каллорий, без физ. активности
+        return "$name : $result kkal"
+
+    }
+}
